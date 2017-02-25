bab40e0c30e2b3480a0b3c299f5705e8961417d7
Cesar Velandia <cesar@velandia.co>
2017-02-25 14:55:58 +0800

    Add enums

--- rust.rs
+++ rust.rs
@@ -164,13 +164,16 @@ fn main() {
 
 	'outer: loop {
 	    let number : i32 = 10;
-	    println!("Pick a number:");
+	    println!("Pick a number (answer=10):");
 
 	    loop {
 	        let mut line = String::new();
 
 	        // line by reference
 	        let input = stdin().read_line(&mut line);
+
+	        //ok(): checks reader at end of input
+	        //map_or()': apply default value or functions to a value
 
 	        let guess: Option<i32> = input.ok().map_or(None, |_| line.trim().parse().ok());
 	    
@@ -192,4 +195,175 @@ fn main() {
 	}
 
 
+	// arrays
+
+	let rand_array = [1,2,3];
+
+	println!("{}", rand_array[0]);
+	println!("{}", rand_array.len());
+	println!("Second 2 {:?}", &rand_array[1..3]);
+
+
+
+	// vectors (grow in size)
+
+	let mut vect1 = vec![1,2,3,4,5];
+
+	println!("Item 2 : {}", vect1[1]);
+
+	for i in &vect1 {
+	    println!("Vect: {}", i );
+	}
+
+	vect1.push(6);
+	vect1.pop();
+
+
+	// tuples (fixed sized lists, keys as indexes)
+
+	let rand_tuple = ("Jim", 30) ;
+	let rand_tuple_2:  (&str, i8) = ("Jim", 30);
+
+	println!("Name: {}", rand_tuple_2.0);
+
+	// functions
+
+	say_hello("Jim");
+	
+	println!("5 + 4 = {}", get_sum(5,4));
+
+	let sum = get_sum;
+	println!("6 + 4 = {}", sum(6,4));
+
+	//closures
+
+	let sum_nums = |x: i32, y: i32| x + y;
+	println!("7 + 8 = {}", sum_nums(7,8));
+
+	let num_ten = 10;
+
+	let add_10 = |x: i32| x + num_ten;
+	println!("5 + 10 = {}", add_10(5));
+
+
+	//ownership
+
+	let vect1 = vec![1,2,3];
+	let vect2 = vect1;
+
+	//wont work, must be used by reference, see below
+	//println!("vect1[0] : {}", vect1[0]);
+
+	let prim_val = 1;
+	let prim_val2 = prim_val;
+
+	//works with primitives only
+	println!("prim_val: {}", prim_val);
+
+	println!("Sum of Vect: {}", sum_vects(&vect2));
+
+	println!("Vect : {:?}", vect2);
+
+	//structs
+
+	let mut circle1 = Circle {
+		x: 10.0, y: 10.0, radius: 10.0
+	};
+
+	println!("X :{}, Y: {}, R: {}", circle1.x, circle1.y, circle1.radius);
+	println!("R: {}", get_radius(&circle1));
+	println!("Circle x: {}", circle1.get_x());
+
+
+	//traits
+
+	println!("Circle area: {}", circle1.area());
+
+	let mut rect1 = Rectangle {
+		height: 10.0, width: 10.0
+	};
+
+	println!("Rectangle Area: {}", rect1.area());
+
+
+	//enumerate
+
+	let hulk = Hero::Strong(100);
+	let quicksilver = Hero::Fast;
+	let spidey = Hero::Info{name: "Spiderman".to_owned(), secret: "Peter".to_owned()};
+
+	print!("hulk"); get_info(hulk);
+	print!("quicksilver"); get_info(quicksilver);
+	get_info(spidey);
+
+
+}
+
+fn say_hello (name: &str){
+
+	println!("Hello {}", name);
+}
+
+fn get_sum(num1: i32, num2: i32) -> i32{
+	num1 + num2
+}
+
+fn sum_vects(v1: &Vec<i32>) -> i32 {
+	// fold appy func to values
+	let sum = v1.iter().fold(0, |mut sum, &x| {sum += x; sum});
+	return sum;
+}
+
+struct Circle {
+	x: f64,
+	y: f64,
+	radius: f64,	
+}
+
+fn get_radius(circle: &Circle) -> f64 {
+	circle.radius
+}
+
+//recommended
+impl Circle {
+	pub fn get_x(&self) -> f64 {
+		self.x
+	} 
+}
+
+struct Rectangle {
+	height: f64,
+	width: f64,
+}
+
+trait HasArea {
+	fn area(&self) -> f64;
+}
+
+impl HasArea for Circle{
+	fn area(&self) -> f64 {
+		3.14159 * (self.radius * self.radius)
+	}
+}
+
+impl HasArea for Rectangle{
+	fn area(&self) -> f64 {
+		self.height * self.width
+	}
+}
+
+enum Hero {
+	Fast, 
+	Strong(i32),
+	Info{name: String, secret: String}
+}
+
+fn get_info(h: Hero){
+	match h {
+	    Hero::Fast => println!(" is Fast"),
+	   	Hero::Strong(i) => println!(" lifts {} tons", i),
+	   	Hero::Info{name, secret} => {
+	   		println!("{} is {}", name, secret);
+	   	},
+	}
 }
